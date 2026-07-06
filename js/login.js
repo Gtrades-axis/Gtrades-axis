43158d9205ed3fed0d2e3d6d9a371c86c9f4c14b
Gtrades-axis <davidthuku574@gmail.com>
2026-07-06 10:33:48 +0300

    Implement user login functionality with Firebase

--- js/login.js
+++ js/login.js
@@ -1,1 +1,82 @@
+import { auth } from "./firebase.js";
 
+import {
+
+signInWithEmailAndPassword,
+
+GoogleAuthProvider,
+
+signInWithPopup
+
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+const googleBtn = document.getElementById("googleLogin");
+
+form.addEventListener("submit", loginUser);
+
+googleBtn.addEventListener("click", googleLogin);
+
+async function loginUser(e){
+
+e.preventDefault();
+
+const email = document.getElementById("email").value.trim();
+
+const password = document.getElementById("password").value;
+
+try{
+
+const userCredential = await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+const user = userCredential.user;
+ import { auth } from "./firebase.js";
+
+import {
+
+signInWithEmailAndPassword,
+
+GoogleAuthProvider,
+
+signInWithPopup
+
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const form = document.getElementById("loginForm");
+
+const googleBtn = document.getElementById("googleLogin");
+
+form.addEventListener("submit", loginUser);
+
+googleBtn.addEventListener("click", googleLogin);
+
+async function loginUser(e){
+
+e.preventDefault();
+
+const email = document.getElementById("email").value.trim();
+
+const password = document.getElementById("password").value;
+
+try{
+
+const userCredential = await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+const user = userCredential.user; 
