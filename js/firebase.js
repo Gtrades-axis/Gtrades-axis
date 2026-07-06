700a16194180b3ff51cbd2b20a3b2689c331c201
Gtrades-axis <davidthuku574@gmail.com>
2026-07-06 11:01:42 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,43 @@
+// ============================
+// GTRADES-AXIS Firebase Config
+// ============================
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+// Firebase Configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBZmsLm64PyEL9jifi32bpgvWfhluIWCZM",
+
+    authDomain: "gtrades-axis.firebaseapp.com",
+
+    projectId: "gtrades-axis",
+
+    storageBucket: "gtrades-axis.firebasestorage.app",
+
+    messagingSenderId: "111456545888",
+
+    appId: "1:111456545888:web:f0526c142d7ea5e22fe705"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+// Authentication
+
+export const auth = getAuth(app);
+
+// Firestore Database
+
+export const db = getFirestore(app);
