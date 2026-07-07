6f24bf8505461574273b7b24a0c81f241bb1aaef
Gtrades-axis <davidthuku574@gmail.com>
2026-07-07 13:13:33 +0300

    Update navbar.js

--- js/js/navbar.js
+++ js/js/navbar.js
@@ -1,23 +1,16 @@
 import { auth } from "./firebase.js";
 
-auth.onAuthStateChanged((user)=>{
+auth.onAuthStateChanged((user) => {
 
-const login=document.querySelector(".login-btn");
-const register=document.querySelector(".register-btn");
-const dashboard=document.getElementById("dashboardBtn");
+    const login = document.getElementById("loginBtn");
+    const dashboard = document.getElementById("dashboardBtn");
 
-if(user){
-
-login.style.display="none";
-register.style.display="none";
-dashboard.style.display="inline-flex";
-
-}else{
-
-login.style.display="inline-flex";
-register.style.display="inline-flex";
-dashboard.style.display="none";
-
-}
+    if (user) {
+        login.style.display = "none";
+        dashboard.style.display = "inline-flex";
+    } else {
+        login.style.display = "inline-flex";
+        dashboard.style.display = "none";
+    }
 
 });
